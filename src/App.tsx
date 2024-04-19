f3e26935d08a758375d620af66e69bc0d465c48b
Victor Hrygorash <viktor12041991@i.ua>
2024-04-19 14:41:37 +0300

    added donation QR code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { About } from "./components/About";
 import { Contact } from "./components/Contact";
+import { Donate } from "./components/Donate";
 import { Footer } from "./components/Footer";
 import Header from "./components/Header";
 import { Projects } from "./components/Projects";
@@ -16,6 +17,7 @@ function App() {
       <About />
       <Projects />
       <Report />
+      <Donate />
       <Contact />
       <Footer/>
     </div>
---
+++ src/components/Donate.tsx
@@ -0,0 +1,19 @@
+export const Donate = () => {
+  return (
+    <section className="donate" id="donate">
+      <div className="container">
+        <div className="donate__inner">
+          <a
+            href="https://send.monobank.ua/jar/3X8W3Dop9C"
+            className="donate__link"
+          >
+            <img src="./img/QR.jpg" alt="qr" />
+          </a>
+          <p className="donate__text">
+            Click on QR code or scan it to donate for our fond.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
